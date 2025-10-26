46f5c1b289679324cebc106fd7de76c73b82cde2
Dhruv-Polaris <dt0557969@gmail.com>
2025-10-26 17:46:57 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -10,5 +13,8 @@ app.use(cors({
 }));
 
 app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
 
 export default app;
--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import{ DB_NAME } from "../constants.js";
+console.log("Connecting to DB with URI:", process.env.MONGODB_URI);
+console.log("DB Name:", DB_NAME);
 
 
 const connectDB = async () => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`/n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        console.log(` MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     }catch(err){
         console.error("Database connection error:", err);
         process.exit(1);
